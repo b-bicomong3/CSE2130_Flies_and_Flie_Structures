79a47d9985cc8ada42c9428bea8ec7345ee5510c
Beatrix Bicomong <b.bicomong@share.epsb.ca>
2022-05-16 01:35:38 -0600

    Best I could do, it's done

--- h_Elk_Island_NP_Grassland_Forest_Ungulate_Population/h_Elk_Island_NP_Grassland_Forest_Ungulate_Population.py
+++ h_Elk_Island_NP_Grassland_Forest_Ungulate_Population/h_Elk_Island_NP_Grassland_Forest_Ungulate_Population.py
@@ -31,11 +31,12 @@ ANIMALS = {
 
 ### --- SUBROUTINES --- ###
 def intro():
-    ''''''
+    """welcomes the user using title
+    """
     print("Welcome to the Elk Island National Park Large Mammal population database!")
 ### --- INPUTS
 def menu():
-    """User selects program action
+    """user selects program action
 
     Returns:
         int:
@@ -54,7 +55,7 @@ def menu():
     return CHOICE 
 
 def getRawData(FILENAME):
-    """Reads CSV File and extracts unprocessed data
+    """reads CSV File and extracts unprocessed data
 
     Args:
         FILENAME (str): 
@@ -74,14 +75,22 @@ def getRawData(FILENAME):
     return CONTENT
 
 def importElk(RAW_DATA):
-    ''''''
+    """creates table and imports data from text file into database table
+
+    Args:
+        RAW_DATA (list):
+    """
     global CURSOR, CONNECTION
 
     RAW_DATA.pop(0)
 
+    for i in range(len(RAW_DATA)):
+        RAW_DATA[i].insert(0, i+1)
+
     CURSOR.execute('''
         CREATE TABLE
             elk_island(
+                id INTEGER,
                 Area_of_park TEXT NOT NULL,
                 Population_year INTEGER NOT NULL,
                 Survey_Year TEXT,
@@ -102,7 +111,7 @@ def importElk(RAW_DATA):
                 Survey_comment TEXT,
                 Estimate_method TEXT,
 
-                PRIMARY KEY(Area_of_park, Population_year, Species_name)
+                PRIMARY KEY(id, Area_of_park, Population_year, Species_name)
                 )
     ;''')
     
@@ -113,16 +122,19 @@ def importElk(RAW_DATA):
             INSERT INTO
                 elk_island
             VALUES (
-                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
             )
         ;''', RAW_DATA[i])
 
     CONNECTION.commit()
     print("Successfully loaded all data!")
 
-
 def askYear():
-    ''''''
+    """asks user input for starting year, end year and animal
+
+    Returns:
+        int: 
+    """
     global CURSOR, ANIMALS
     
     # inputs
@@ -211,29 +223,54 @@ def askYear():
     if YEAR_END_N == list(EMPTY) or YEAR_END_S == list(EMPTY) or END != YEAR_END_N[0][1] or END != YEAR_END_S[0][1]:
         print("One of the inputted years does not exist in our database, re-enter year or add year to data")
         print("Please make sure both North and South areas are available from that year")
+        print("If the user wishes to continue, data might be inaccurate")
         print('''
     1. Re-enter year
     2. Add year
+    3. Continue
         ''')
         CHOICE= input("> ")
         CHOICE = int(CHOICE)
         if CHOICE == 1:
             return askYear()
         elif CHOICE == 2:
-            return addData()
+            return addDataAsk()
+        else:
+            pass
 
-    if "North" != YEAR_START_N[0][0] or "North" not in YEAR_END_N[0][0]:
+    try:
+        if "North" not in YEAR_START_N[0][0]:
+            YEAR_START_N = 0
+    except:
         YEAR_START_N = 0
+    try:        
+        if "North" not in YEAR_END_N[0][0]:
+            YEAR_END_N = 0
+    except:
         YEAR_END_N = 0
-
-    if "South" not in YEAR_START_S[0][0] or "South" not in YEAR_END_S[0][0]:
-        YEAR_START_S = 0
+    try:        
+        if "South" not in YEAR_START_S[0]:
+            YEAR_START_S = 0
+    except:
+        YEAR_START_S = 0  
+    try:      
+        if "South" not in YEAR_END_S[0]:
+            YEAR_END_S = 0
+    except:
         YEAR_END_S = 0
 
-    return YEAR_START_N, YEAR_END_N, YEAR_START_S, YEAR_END_S
+    return YEAR_START_N, YEAR_END_N, YEAR_START_S, YEAR_END_S, START, END
 
 def animalMult(START, END):
-    ''''''
+    """selects data of all animals
+
+    Args:
+        START (int): 
+        END (int): 
+
+    Returns:
+        list: 
+    """
 
     YEAR_START_N = CURSOR.execute('''
         SELECT
@@ -300,17 +337,11 @@ def animalMult(START, END):
     EMPTY = []
 
     # outputs 
-
-    if "North" != YEAR_START_N[0][0] or "North" not in YEAR_END_N[0][0]:
-        YEAR_START_N = 0
-        YEAR_END_N = 0
-
-    if "South" not in YEAR_START_S[0][0] or "South" not in YEAR_END_S[0][0]:
-        YEAR_START_S = 0
-        YEAR_END_S = 0
-
     if YEAR_END_N == list(EMPTY) or YEAR_END_S == list(EMPTY) or END != YEAR_END_N[0][1] or END != YEAR_END_S[0][1]:
-        print(f"One of the inputted years does not exist in our database, re-enter year or add year to data")
+        print("One of the inputted years does not exist in our database, re-enter year or add year to data")
+        print("Please make sure both North and South areas are available from that year")
+        print("As well as all animals from each area")
+        print("Continuing is not a feature for selecting all populations")
         print('''
     1. Re-enter year
     2. Add year
@@ -320,12 +351,83 @@ def animalMult(START, END):
         if CHOICE == 1:
             return askYear()
         elif CHOICE == 2:
-            return addData()
+            return addDataAsk()
 
     return YEAR_START_N, YEAR_END_N, YEAR_START_S, YEAR_END_S
 
+def addDataAsk():
+    """adds inputted data from user into database by selecting through the growth population option
+
+    Returns:
+        int: 
+    """
+    global CURSOR, CONNECTION
+
+    # inputs
+    AREA_OF_PARK = input("Area of park: ")
+    POPULATION_YEAR = input("Population year: ")
+    SURVEY_YEAR = input("Survey Year: ")
+    SURVEY_MONTH = input("Survey Month: ")
+    SURVEY_DAY = input("Survey Day: ")
+    SPECIES_NAME = input("Species name: ")
+    UNKNOWN_AGE_AND_SEX_COUNT = input("Unknown age and sex count: ")
+    ADULT_MALE_COUNT = input("Adult male count: ")
+    ADULT_FEMALE_COUNT = input("Adult female count: ")
+    ADULT_UNKNOWN_COUNT = input("Adult unknown count: ")
+    YEARLING_COUNT = input("Yearling count: ")
+    CALF_COUNT = input("Calf count: ")
+    SURVEY_TOTAL = input("Survey Total: ") 
+    SIGHTIBILTY_CORRECTION_FACTOR = input("Sightibility correction factor: ")
+    ADDITIONAL_CAPTIVE_COUNT = input("Additional captive count: ")
+    ANIMALS_REMOVED_PRIOR_TO_SURVEY = input("Animals removed prior to suvrey: ")
+    FALL_POPULATION_ESTIMATE = input("Fall population estimate: ")
+    SURVEY_COMMENT = input("Survey comment: ")
+    ESTIMATE_METHOD= input("Estimate method: ")
+
+    # processing 
+    if AREA_OF_PARK == "" or POPULATION_YEAR == "":
+        print("Not enough information given")
+    else:
+        CURSOR.execute('''
+            INSERT INTO
+                elk_island(
+                    Area_of_park,
+                    Population_year,
+                    Survey_Year,
+                    Survey_Month,
+                    Survey_Day,
+                    Species_name,
+                    Unknown_age_and_sex_count,
+                    Adult_male_count,
+                    Adult_female_count,
+                    Adult_unknown_count,
+                    Yearling_count,
+                    Calf_count,
+                    Survey_total,
+                    Sightability_correction_factor,
+                    Additional_captive_count,
+                    Animals_removed_prior_to_survey,
+                    Fall_population_estimate,
+                    Survey_comment,
+                    Estimate_method
+                    )
+                VALUES (
+                    ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+                    )
+        ;''', (AREA_OF_PARK, POPULATION_YEAR, SURVEY_YEAR, SURVEY_MONTH, SURVEY_DAY, SPECIES_NAME, 
+        UNKNOWN_AGE_AND_SEX_COUNT, ADULT_MALE_COUNT, ADULT_FEMALE_COUNT, ADULT_UNKNOWN_COUNT, YEARLING_COUNT,
+        CALF_COUNT, SURVEY_TOTAL, SIGHTIBILTY_CORRECTION_FACTOR, ADDITIONAL_CAPTIVE_COUNT, 
+        ANIMALS_REMOVED_PRIOR_TO_SURVEY, FALL_POPULATION_ESTIMATE, SURVEY_COMMENT, ESTIMATE_METHOD))
+
+    # output
+        CONNECTION.commit()
+        print(f"Year {POPULATION_YEAR} successfully saved to contacts!")
+
+    return askYear()
+
 def addData():
-    ''''''
+    """adds inputted data from user into database
+    """
     global CURSOR, CONNECTION
 
     # inputs
@@ -385,17 +487,20 @@ def addData():
         ANIMALS_REMOVED_PRIOR_TO_SURVEY, FALL_POPULATION_ESTIMATE, SURVEY_COMMENT, ESTIMATE_METHOD))
 
     # output
-    CONNECTION.commit()
-    print(f"Year {POPULATION_YEAR} successfully saved to contacts!")
-
-    return askYear()
+        CONNECTION.commit()
+        print(f"Year {POPULATION_YEAR} successfully saved to contacts!")
 
 def getDataID():
-    ''''''
+    """gets the data's id number
+
+    Returns:
+        int: id
+    """
     global CURSOR
 
     DATA = CURSOR.execute('''
         SELECT
+            id,
             Area_of_park,
             Population_year,
             Species_name,
@@ -408,21 +513,30 @@ def getDataID():
     ;''').fetchall()
     print("Please select a contact")
     for i in range(len(DATA)):
-        print(f"> {DATA[i][0]} {DATA[i][1]} {DATA[i][2]} {DATA[i][3]}")
+        print(f"{i+1}. {DATA[i][1]} {DATA[i][2]} {DATA[i][3]} {DATA[i][4]}")          
 
-    print("Please enter the area, year and animal of data: ")
+    ROW_INDEX = input("> ")
+    ROW_INDEX = int(ROW_INDEX) - 1
 
-    AREA_ID = input("Area: ")
-    YEAR_INDEX = input("Year: ")
-    YEAR_ID = int(YEAR_INDEX)
-    ANIMAL_ID = input("Animal: ")
-
-    return AREA_ID, YEAR_ID, ANIMAL_ID
+    DATA_ID = DATA[ROW_INDEX][0]
+    return DATA_ID
 
 ### --- PROCESSINGS
 
-def calcPopulation(YEAR_START_N, YEAR_END_N, YEAR_START_S, YEAR_END_S):
-    ''''''
+def calcPopulation(YEAR_START_N, YEAR_END_N, YEAR_START_S, YEAR_END_S, YEAR_1, YEAR_2):
+    """calculates the population of one or all animals from the inputted years
+
+    Args:
+        YEAR_START_N (int or list): beginning year population
+        YEAR_END_N (int or list): end year population
+        YEAR_START_S (int or list): beginning year population
+        YEAR_END_S (int or list): end year population
+        YEAR_1 (int): beginning year
+        YEAR_2 (int): end year
+
+    Returns:
+        _type_: _description_
+    """
     if len(YEAR_START_N) == 4:
         NUMBER_1_N = []
         NUMBER_2_N = []
@@ -447,15 +561,43 @@ def calcPopulation(YEAR_START_N, YEAR_END_N, YEAR_START_S, YEAR_END_S):
         NUMBER_1_S = 0
         NUMBER_2_S = 0
     else:
-        NUMBER_1_N = YEAR_START_N[0][3]
-        NUMBER_2_N = YEAR_END_N[0][3]
-        NUMBER_1_S = YEAR_START_S[0][3]
-        NUMBER_2_S = YEAR_END_S[0][3]
+        try:
+            NUMBER_1_N = YEAR_START_N[0][3]
+        except:
+            NUMBER_1_N = YEAR_START_N
+        try:
+            NUMBER_2_N = YEAR_END_N[0][3]
+        except:
+            NUMBER_2_N = YEAR_END_N
+        try:
+            NUMBER_1_S = YEAR_START_S[0][3]
+        except:
+            NUMBER_1_S = YEAR_START_S
+        try:
+            NUMBER_2_S = YEAR_END_S[0][3]
+        except:
+            NUMBER_2_S = YEAR_END_S
 
-    NUMBER_YEAR_1 = YEAR_START_N[0][1]
-    NUMBER_YEAR_2 = YEAR_END_N[0][1]
+    NUMBER_YEAR_1 = YEAR_1
+    NUMBER_YEAR_2 = YEAR_2
+
+    try:
+        if len(NUMBER_1_N) < 4:
+            for i in range(len(NUMBER_1_N)):
+                NUMBER_1_N.append(0)
+        elif len(NUMBER_2_N) < 4:
+            for i in range(len(NUMBER_2_N)):
+                NUMBER_2_N.append(0)
+        elif len(NUMBER_1_S) < 4:
+            for i in range(len(NUMBER_1_S)):
+                NUMBER_1_S.append(0)
+        elif len(NUMBER_2_S) < 4:
+            NUMBER_2_S.append(0)
+    except:
+        pass
 
     if len(YEAR_START_N) == 4:
+
         CALC_B_1 = NUMBER_2_N[0] + NUMBER_2_S[0]
         CALC_E_1 = NUMBER_2_N[2] + NUMBER_2_S[2]
         CALC_M_1 = NUMBER_2_N[3] + NUMBER_2_S[3]
@@ -491,7 +633,7 @@ def calcPopulation(YEAR_START_N, YEAR_END_N, YEAR_START_S, YEAR_END_S):
         SUM = SUM_B, SUM_E, SUM_M, SUM_D
         SUM_ANIMAL = SUM_ANIMAL_B, SUM_ANIMAL_E, SUM_ANIMAL_M, SUM_ANIMAL_D
 
-        return dispAll(SUM, SUM_ANIMAL)
+        return SUM, SUM_ANIMAL
     else:
         CALC_1 = NUMBER_2_N + NUMBER_2_S
         CALC_2 = NUMBER_1_N + NUMBER_1_S
@@ -508,9 +650,12 @@ def calcPopulation(YEAR_START_N, YEAR_END_N, YEAR_START_S, YEAR_END_S):
 
     return SUM, SUM_ANIMAL
 
+def deleteData(ID):
+    """deletes data using id
 
-def deleteData(AREA_ID, YEAR_ID, ANIMAL_ID):
-    ''''''
+    Args:
+        ID (int):
+    """
     global CURSOR, CONNECTION
 
     DELETE = CURSOR.execute('''
@@ -522,37 +667,39 @@ def deleteData(AREA_ID, YEAR_ID, ANIMAL_ID):
         FROM 
             elk_island
         WHERE
-            Population_year = ? and
-            Species_name = ? and
-            Area_of_park = ?
-    ;''', [AREA_ID, YEAR_ID, ANIMAL_ID]).fetchone()
+            id = ?
+    ;''', [ID]).fetchone()
 
     # DELETE
     CURSOR.execute('''
         DELETE FROM
             elk_island
         WHERE
-            Area_of_park = ? and
-            Population_year = ? and
-            Species_name = ?
-    ;''', [AREA_ID, YEAR_ID, ANIMAL_ID])
+            id = ?
+    ;''', [ID])
 
     CONNECTION.commit()
-
-    print("%s %s %s %s successfully deleted" % DELETE)
+    try:
+        print("%s %s %s %s successfully deleted" % DELETE)
+    except:
+        print("unidentifiable id")
 
 ### --- OUTPUTS
 
 def diplayResult(SUM, SUM_ANIMAL):
-    ''''''
-    print(f"{SUM} {SUM_ANIMAL}/yr")
-    
-def dispAll(SUM, SUM_ANIMAL):
-    ''''''
-    for i in range(len(SUM)):
-        print(f"{SUM[i]} {SUM_ANIMAL[i]}/yr")
+    """displyas the results for the user
 
-    return menu()
+    Args:
+        SUM (int or list): 
+        SUM_ANIMAL (int or list): 
+    """
+    try: 
+        if len(SUM) == 4:
+            for i in range(len(SUM)):
+                print(f"{SUM[i]} {SUM_ANIMAL[i]}/yr")   
+    except:    
+        print(f"{SUM} {SUM_ANIMAL}/yr")
+    
 
 ### --- MAIN PROGRAM --- ###
 
@@ -564,15 +711,14 @@ if __name__ == "__main__":
     while True:
         CHOICE = menu()
         if CHOICE == 1:
-            DATA_1, DATA_2, DATA_3, DATA_4 = askYear()
-            RESULT, RESULT_ANIMAL = calcPopulation(DATA_1, DATA_2, DATA_3, DATA_4)
+            DATA_1, DATA_2, DATA_3, DATA_4, YEAR_1, YEAR_2 = askYear()
+            RESULT, RESULT_ANIMAL = calcPopulation(DATA_1, DATA_2, DATA_3, DATA_4, YEAR_1, YEAR_2)
             diplayResult(RESULT, RESULT_ANIMAL)
         elif CHOICE == 2:
             addData()
         elif CHOICE == 3:
-            AREA_ID, YEAR_ID, ANIMAL_ID = getDataID()
-            deleteData(AREA_ID, YEAR_ID, ANIMAL_ID)
+            ID = getDataID()
+            deleteData(ID)
         elif CHOICE == 4:
-            exit
-        else:
-            pass
+            print("Thank you for using Elk Island National Park Large Mammal population database!")
+            exit()
